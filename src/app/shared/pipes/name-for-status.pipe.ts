b08315375f7c7146c3d5ec4e6f351270b9f01a4e
Lika <bartuzanovaan@iss-reshetnev.ru>
2022-07-26 14:51:11 +0700

    feat(pipe): Изменен пайп для отображения статусов

--- src/app/shared/pipes/name-for-status.pipe.ts
+++ src/app/shared/pipes/name-for-status.pipe.ts
@@ -4,17 +4,9 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "nameForStatus"
 })
 export class NameForStatusPipe implements PipeTransform {
-  transform(number: number): any {
-    switch (number) {
-      case 0:
-        return "Не доставлено";
-      case 1:
-        return "Доставлено";
-      case 2:
-        return "Возврат";
-      case 3:
-        return "Возвращено";
-    }
+  transform(number: number, statuses: any): any {
+    let object = statuses.find((obj: any) => obj.id === number);
 
+    return object.name;
   }
 }
